6d7fbd23a536c59764ecf62e40087e228da2c03b
Jovic Abengona <jvlabengona@gmail.com>
2021-06-28 18:29:38 +0800

    Dojo Dashboard II Commit

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -2,4 +2,22 @@ class DojosController < ApplicationController
   def index
     @dojos = Dojo.all
   end
+
+  def new
+  end
+
+  def create
+    dojos = Dojo.create(dojo_params)
+    if dojos.valid?
+      redirect_to '/'
+    else
+      flash[:errors] = dojos.errors.full_messages
+      redirect_to '/dojos/new'
+    end
+  end
+
+  private
+    def dojo_params
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'dojos/index'
+  get 'dojos/new'
+
+  post 'dojos' => 'dojos#create'
 
   root 'dojos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
